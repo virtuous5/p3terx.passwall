d770393ce3c4ec805c7c7e8fb019f9c099f27098
virtuous5 <62429535+virtuous5@users.noreply.github.com>
2020-04-07 22:17:25 +0800

    Initial commit

---
+++ diy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#=================================================
+# Description: DIY script
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=================================================
+# Modify default IP
+sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
+git clone https://github.com/Lienol/openwrt-package package/lienol
